0e902a95d954d86c43d7d360e39f622ffdebfed2
irala <nineirm@gmail.com>
2020-06-27 20:46:28 +0200

    change build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-g++ -std=c++17 -g source/*.cpp
+#g++ -std=c++17 -g source/*.cpp
+
+# Default number of cores to use if not specified via comnad line
+CORES=$(nproc --all)
+# Directory where the application will be built
+BUILDDIR=build
+# clean
+CLEAN=
+
+# create the build directory for first time
+if [ ! -d  "$BUILDDIR" ]; then
+  mkdir $BUILDDIR
+fi
+
+if [[ "$2" == "clean" ]]; then
+  CLEAN=$2
+fi
+cd $BUILDDIR
+
+# Build the application
+
+if [ -n "$1" ]; then
+  CORES=$1
+fi
+
+if [ -x "$(command -v clang)" ]; then
+  export CC=/usr/bin/clang
+  export CXX=/usr/bin/clang++
+  echo 'Using Clang'
+fi
+
+cmake  ..
+if [[ -n "$CLEAN" ]]; then
+  make clean
+fi
+
+
+make -j $CORES
